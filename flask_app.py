b3e14df4fada557cd1ad118cec7aac8f745ec980
stuntman723 <ryan@stuntz.org>
2015-09-30 17:25:59 -0400

    Added new homepage

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,18 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
+
+
+DEBUG = True
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    # return "fdsfds"
+    return render_template("base.html")
 
+if __name__ == '__main__':
+    app.run()
